ebecd18d2b150ef10423a5ea2ba66681c6c7cce6
Kamil <kamilwronka7@gmail.com>
2018-11-25 23:57:51 +0100

    idk

--- server/messages/messages.controller.js
+++ server/messages/messages.controller.js
@@ -1,18 +1,17 @@
 const Message = require("./messages.model");
 
 module.exports = (app, io) => {
-    io.on('connection', function(socket) {
-        console.log('connected')
-        socket.on('message', function(msg) {
-            const message = new Message({...msg, date: new Date().toISOString()});     
-            message.save().then((msg) => 
-            socket.emit('message', msg)
-            )
-        })
-      })
-      app.get('/api/messages', (req, res) => {
-          Message.find().then(messages => {
-              res.send(messages);
-          })
-      })
-}
+  io.on("connection", function(socket) {
+    console.log("connected");
+    socket.on("message", function(msg) {
+      const message = new Message({ ...msg, date: new Date().toISOString() });
+      console.log(msg);
+      message.save().then(msg => io.emit("message", msg));
+    });
+  });
+  app.get("/api/messages", (req, res) => {
+    Message.find().then(messages => {
+      res.send(messages);
+    });
+  });
+};
