948d01985ac70ab5fc6e63fab4bfb384b50c0b0f
Kamil Wronka <Kamil.Wronka@rossmann.com.pl>
2019-01-30 14:26:43 +0100

    mongo url

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/messenger";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://root:Kamilo108@ds139690.mlab.com:39690/messenger-backend1337";
 
 mongoose.connect(
   `${MONGODB_URI}`,
