6ff65873dc4352c45c88ba255996787884c0eefe
Kamil <kamilwronka7@gmail.com>
2019-03-23 20:06:54 +0100

    dockerfile update

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,12 @@
 const production = process.env.NODE_ENV === "production";
 const devKeys = require("./keys_dev");
 
+console.log(
+  process.env.AWS_ACCESS,
+  process.env.AWS_SECRET,
+  process.env.MONGODB_URI
+);
+
 module.exports = {
   AWS_ACCESS: production ? process.env.AWS_ACCESS : devKeys.AWS_ACCESS,
   AWS_SECRET: production ? process.env.AWS_SECRET : devKeys.AWS_SECRET
